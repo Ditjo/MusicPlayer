9afd1e9fb6151fb2ac465178980e5ba96c1de8d7
Ditjo <mads.ditjo@gmail.com>
2024-05-16 22:12:03 +0200

    -Changed Song to public

--- MusicPlayer/Data/Models/Song.cs
+++ MusicPlayer/Data/Models/Song.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace MusicPlayer.Data.Models
 {
-    class Song
+    public class Song
     {
         public int Id { get; set; }
-        public string Url { get; set; }
-        public string SongName { get; set; }
-        public string Artist {  get; set; }
+        public string? Url { get; set; }
+        public string? SongName { get; set; }
+        public string? Artist {  get; set; }
         public DateTime Length { get; set; }
     }
 }
