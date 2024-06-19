c9a03714c92420acde1e1ae1bfc867889fdb3445
Ditjo <mads.ditjo@gmail.com>
2024-06-19 21:02:18 +0200

    -Added Converter

---
+++ MusicPlayer/UI/Converters/Converters.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MusicPlayer.UI.Converters
+{
+    public class TimeSpanToDoubleConverter : IValueConverter
+    {
+        public enum TimeSpanFormat
+        {
+            TotalSeconds,
+            TotalMinutes,
+            TotalHours,
+            TotalDays
+        }
+
+        public TimeSpanFormat Format { get; set; } = TimeSpanFormat.TotalSeconds;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is TimeSpan timeSpan)
+            {
+                switch (Format)
+                {
+                    case TimeSpanFormat.TotalSeconds:
+                        return timeSpan.TotalSeconds;
+                    case TimeSpanFormat.TotalMinutes:
+                        return timeSpan.TotalMinutes;
+                    case TimeSpanFormat.TotalHours:
+                        return timeSpan.TotalHours;
+                    case TimeSpanFormat.TotalDays:
+                        return timeSpan.TotalDays;
+                    default:
+                        return timeSpan.TotalSeconds;
+                }
+            }
+
+            return 0.0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double doubleValue)
+            {
+                switch (Format)
+                {
+                    case TimeSpanFormat.TotalSeconds:
+                        return TimeSpan.FromSeconds(doubleValue);
+                    case TimeSpanFormat.TotalMinutes:
+                        return TimeSpan.FromMinutes(doubleValue);
+                    case TimeSpanFormat.TotalHours:
+                        return TimeSpan.FromHours(doubleValue);
+                    case TimeSpanFormat.TotalDays:
+                        return TimeSpan.FromDays(doubleValue);
+                    default:
+                        return TimeSpan.FromSeconds(doubleValue);
+                }
+            }
+
+            return TimeSpan.Zero;
+        }
+    }
+}
