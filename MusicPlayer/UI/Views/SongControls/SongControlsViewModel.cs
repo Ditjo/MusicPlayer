6ddec4f116952a21ec2543b6c0313e12fc43dbaf
Ditjo <mads.ditjo@gmail.com>
2024-06-21 19:45:59 +0200

    -Added SongControl View & ViewModel.

---
+++ MusicPlayer/UI/Views/SongControls/SongControlsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MusicPlayer.UI.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer.UI.Views.SongControls
+{
+    public class SongControlsViewModel : ViewModelBase
+    {
+
+    }
+}
