a750802a2cfc21b720d7ad155fd4a5f99ad360d7
Ditjo <mads.ditjo@gmail.com>
2024-07-02 21:39:24 +0200

    -Removed old code. -Set Start page to Home.

--- MusicPlayer/UI/Views/MainView/MainViewModel.cs
+++ MusicPlayer/UI/Views/MainView/MainViewModel.cs
@@ -25,19 +25,10 @@ namespace MusicPlayer.UI.Views.MainView
 {
     internal class MainViewModel : ViewModelBase
     {
-        //private bool musicNotStoppedByPerson = true;
-        //private WaveOutEvent? outputDevice;
-        //private readonly DispatcherTimer musicTimer;
-        //private AudioFileReader? audioFile;
-
         private static string staticPath = "";
         FileWorker fileWorker;
 
         public ICommand NavigationCommand { get; set; }
-        //public ICommand PlayMusicCommand { get; set; }
-        //public ICommand StopMusicCommand { get; set; }
-        //public ICommand RewindMusicCommand { get; set; }
-        //public ICommand SkipTimeInMusicCommand { get; set; }
 
         public MainViewModel()
         {
@@ -48,22 +39,11 @@ namespace MusicPlayer.UI.Views.MainView
             LoadData();
 
             NavigationCommand = new RelayCommand<object>(OnNavigateCommand);
-            //PlayMusicCommand = new RelayCommand(PlayMusic, CanPlayMusic);
-            //StopMusicCommand = new RelayCommand(StopMusic);
-            //RewindMusicCommand = new RelayCommand(OnRewindMusicCommand);
-            //SkipTimeInMusicCommand = new RelayCommand<object>(OnSkipTimeInMusicCommand);
 
             SongControls = new SongControlsViewModel();
 
             //Sets Starting Page
-            RequestForNavigation("Playlist");
-
-            //musicTimer = new DispatcherTimer();
-            //musicTimer.Interval = TimeSpan.FromMilliseconds(500);
-            //musicTimer.Tick += TimerTick;
-
-            //OutputVolume = 100;
-            //PlayBtn = "Play";
+            RequestForNavigation("Home");
         }
 
         internal void RequestForNavigation(string obj, object? entity = null)
@@ -77,7 +57,7 @@ namespace MusicPlayer.UI.Views.MainView
             ViewModelBase viewModel;
             switch (obj)
             {
-                case "FrontPage":
+                case "Home":
                     viewModel = new FrontPageViewModel();
                     viewModel.RequestForNavigationEvent += RequestForNavigation;
                     viewModel.Header = "Home";
@@ -127,122 +107,6 @@ namespace MusicPlayer.UI.Views.MainView
         {
             Songs = fileWorker.GetAllSongs();
         }
-
-        //public void PlayMusic()
-        //{
-        //    Debug.WriteLine("Playing Music...");
-        //    if (outputDevice == null) return;
-
-        //    if (outputDevice.PlaybackState == PlaybackState.Playing)
-        //    {
-        //        musicNotStoppedByPerson = false;
-        //        outputDevice?.Stop();
-        //        musicTimer.Stop();
-        //        PlayBtn = "Play";
-        //    }
-        //    else
-        //    {
-        //        outputDevice?.Play();
-        //        musicTimer.Start();
-        //        PlayBtn = "Stop";
-        //    }
-        //}
-
-        //public bool CanPlayMusic()
-        //{
-        //    return SelectedMusic is not null && File.Exists(SelectedMusic.Url);
-        //}
-
-        //public void StopMusic()
-        //{
-        //    Debug.WriteLine("Stopped Music");
-
-        //    musicNotStoppedByPerson = false;
-        //    outputDevice?.Stop();
-        //    musicTimer.Stop();
-        //}
-
-        //public void OnRewindMusicCommand()
-        //{
-        //    if (audioFile == null) return;
-        //    audioFile.Position = 0;
-        //}
-
-        //public void OnSkipTimeInMusicCommand(object sec)
-        //{
-        //    if (audioFile != null && sec != null)
-        //    {
-        //        audioFile.Skip(int.Parse(sec.ToString()));
-        //    }
-        //}
-
-        //public void SetTime(TimeSpan time)
-        //{
-        //    if (audioFile != null)
-        //    {
-        //        audioFile.CurrentTime = time;
-        //    }
-        //}
-
-        //private void OnPlaybackStopped(object sender, StoppedEventArgs args)
-        //{
-        //    //If music stopped on it's own. CleanPlayback.
-        //    //If more in playlist play that.
-        //    if (musicNotStoppedByPerson)
-        //    {
-        //        CleanPlayback();
-        //    }
-        //    PlayBtn = "Play";
-        //    musicNotStoppedByPerson = true;
-        //}
-
-        ////
-        //public void CleanPlayback()
-        //{
-
-        //    if (outputDevice != null)
-        //    {
-        //        if (outputDevice.PlaybackState == PlaybackState.Playing)
-        //        {
-        //            outputDevice.PlaybackStopped -= OnPlaybackStopped;
-        //            PlayBtn = "Play";
-        //            outputDevice.Stop();
-        //        }
-        //        outputDevice.Dispose();
-        //        outputDevice = null;
-        //    }
-        //    if (audioFile != null)
-        //    {
-        //        audioFile.Dispose();
-        //        audioFile = null;
-        //    }
-        //}
-
-        //public void SelectedSongInit()
-        //{
-        //    CleanPlayback();
-        //    if (outputDevice == null)
-        //    {
-        //        outputDevice = new WaveOutEvent();
-        //        outputDevice.PlaybackStopped += OnPlaybackStopped;
-        //    }
-        //    if (audioFile == null)
-        //    {
-        //        audioFile = new AudioFileReader(SelectedMusic?.Url);
-        //        outputDevice.Init(audioFile);
-        //    }
-        //    TimeOffSong = audioFile is null ? TimeSpan.Zero : audioFile.TotalTime;
-        //}
-
-
-        //public bool isDragging = false;
-        //private void TimerTick(object sender, EventArgs e)
-        //{
-        //    if (audioFile is not null && !isDragging)
-        //    {
-        //        CurrentTime = audioFile.CurrentTime;
-        //    }
-        //}
 
         #endregion
 
@@ -316,87 +180,6 @@ namespace MusicPlayer.UI.Views.MainView
                 }
             }
         }
-
-        //private string _playBtn;
-
-        //public string PlayBtn
-        //{
-        //    get
-        //    {
-        //        return _playBtn;
-        //    }
-        //    set
-        //    {
-        //        if (_playBtn != value)
-        //        {
-        //            _playBtn = value;
-        //            OnPropertyChanged();
-        //        }
-        //    }
-        //}
-
-
-        //private TimeSpan _timeOffSong;
-        //public TimeSpan TimeOffSong
-        //{
-        //    get
-        //    {
-        //        return _timeOffSong;
-        //    }
-        //    set
-        //    {
-        //        if (_timeOffSong != value)
-        //        {
-        //            _timeOffSong = value;
-        //            OnPropertyChanged();
-        //        }
-        //    }
-        //}
-
-        //private TimeSpan _currentTime;
-        //public TimeSpan CurrentTime
-        //{
-        //    get
-        //    {
-        //        return _currentTime;
-        //    }
-        //    set
-        //    {
-        //        if (_currentTime != value)
-        //        {
-
-        //            _currentTime = value;
-        //            OnPropertyChanged();
-        //        }
-        //    }
-        //}
-
-        //private float _outputVolume;
-        //public float OutputVolume
-        //{
-        //    get
-        //    {
-        //        return _outputVolume;
-        //    }
-        //    set
-        //    {
-        //        if (_outputVolume != value)
-        //        {
-        //            //if(audioFile != null)
-        //            //{
-        //            //    audioFile.Volume = value / 100f;
-        //            //}
-
-        //            if (outputDevice != null)
-        //            {
-        //                outputDevice.Volume = value / 100f;
-        //            }
-
-        //            _outputVolume = value;
-        //            OnPropertyChanged();
-        //        }
-        //    }
-        //}
 
         #endregion
 
