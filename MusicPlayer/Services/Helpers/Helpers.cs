98535fb10dad2815ef15ed142c93713308ef4caf
Ditjo <mads.ditjo@gmail.com>
2024-07-08 17:46:56 +0200

    -add Helper Class with ToObservableCollection

---
+++ MusicPlayer/Services/Helpers/Helpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer.Services.Helpers
+{
+    public static class ToObservableCollectionExtension
+    {
+        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> source)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            return new ObservableCollection<T>(source);
+        }
+    }
+}
