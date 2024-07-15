08323362bbf888d18bf0c0ae636bb769609bc44b
Ditjo <mads.ditjo@gmail.com>
2024-07-15 14:28:21 +0200

    -Added FileHandler to save and load json

---
+++ MusicPlayer/Services/Helpers/FileHandler.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer.Services.Helpers
+{
+    public class FileHandler
+    {
+        private static string _fileLocation = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        public static void SaveToJSON<T>(T obj, string filename)
+        {
+            var fullPath = Path.Combine(_fileLocation, filename);
+
+            string json = JsonConvert.SerializeObject(obj, Formatting.Indented);
+            File.WriteAllText(fullPath, json);
+        }
+
+        public static T? LoadFromJSON<T>(string filename)
+        {
+            var fullPath = Path.Combine(_fileLocation, filename);
+
+            if (File.Exists(fullPath))
+            {
+                string json = File.ReadAllText(fullPath);
+                return JsonConvert.DeserializeObject<T>(json);
+            }
+            return default;
+        }
+
+    }
+}
