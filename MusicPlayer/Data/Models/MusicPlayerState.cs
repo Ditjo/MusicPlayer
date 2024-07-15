ff4dd3ee59cfb65f9ca648dc87d6dca4b67a664e
Ditjo <mads.ditjo@gmail.com>
2024-07-15 09:18:38 +0200

    -Added MusicPlayerState Model

---
+++ MusicPlayer/Data/Models/MusicPlayerState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer.Data.Models
+{
+    public class MusicPlayerState
+    {
+        public Song CurrentSong { get; set; }
+        public Queue<Song> SongQueue { get; set; }
+        public Stack<Song> PastSong { get; set; }
+
+        public MusicPlayerState()
+        {
+            SongQueue = new Queue<Song>();
+            PastSong = new Stack<Song>();
+        }
+    }
+}
