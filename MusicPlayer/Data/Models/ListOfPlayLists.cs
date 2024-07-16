ec0add70dcc28dc19515dfa4e6f59764d63a3c03
Ditjo <mads.ditjo@gmail.com>
2024-07-16 22:38:02 +0200

    -Added PlayList Models

---
+++ MusicPlayer/Data/Models/ListOfPlayLists.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer.Data.Models
+{
+    public class ListOfPlayLists
+    {
+        public List<PlayList> PlayLists { get; set; }
+    }
+}
---
+++ MusicPlayer/Data/Models/PlayList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer.Data.Models
+{
+    public class PlayList
+    {
+        public PlayList(string title)
+        {
+            Title = title;
+            Songs = new();
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Queue<Song> Songs { get; set;}
+    }
+}
