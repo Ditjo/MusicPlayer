b3b6481570cc8d0696903a40ed2ee454b46dce51
Ditjo <mads.ditjo@gmail.com>
2024-07-24 23:28:17 +0200

    -Added NewPlayList View & ViewModel

---
+++ MusicPlayer/UI/Views/NewPlaylist/NewPlaylistViewModel.cs
@@ -0,0 +1,36 @@
+﻿using MusicPlayer.UI.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer.UI.Views.NewPlaylist
+{
+    public class NewPlaylistViewModel : ViewModelBase
+    {
+        public NewPlaylistViewModel()
+        {
+
+        }
+
+        private string? _name;
+
+		public string? Name
+		{
+			get 
+			{
+				return _name; 
+			}
+			set 
+			{
+				if (_name != value)
+				{
+					_name = value; 
+					OnPropertyChanged();
+				}
+			}
+		}
+
+	}
+}
