cd7c6e8bcaeff0788556208f778d88b2b2600f5c
Ditjo <mads.ditjo@gmail.com>
2024-08-03 16:21:15 +0200

    -Added AddSongToPlaylist dialog

---
+++ MusicPlayer/UI/Views/AddSongToPlaylist/AddSongToPlaylistView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MusicPlayer.UI.Views.AddSongToPlaylist
+{
+    /// <summary>
+    /// Interaction logic for AddSongToPlaylistView.xaml
+    /// </summary>
+    public partial class AddSongToPlaylistView : UserControl
+    {
+        public AddSongToPlaylistView()
+        {
+            InitializeComponent();
+        }
+    }
+}
